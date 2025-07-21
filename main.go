8f508e69f837bd40110d1467e8e5451d0cd91f35
ClemGamer <tsaiyouchen@gmail.com>
2025-07-21 20:49:22 +0800

    feat: add '/healthz' api for health check and separate filepath

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func main() {
 
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	server := &http.Server{
 		Addr:    ":" + port,
